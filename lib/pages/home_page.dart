c2013edf0a102bf0c273a5960ca74f6fc506c765
Mr.Perfect <ziyodovanvar1999@gmail.com>
2020-11-04 07:12:42 -0600

    Introduction page has been completed

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,8 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_covid_app/core/consts.dart';
+import 'package:flutter_covid_app/core/flutter_icons.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,6 +12,64 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: AppColors.backgroundColor,
+      body: Column(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+              color: AppColors.mainColor,
+              borderRadius: BorderRadius.only(
+                bottomLeft: Radius.circular(20),
+                bottomRight: Radius.circular(20),
+              ),
+            ),
+            padding: EdgeInsets.only(top: 25),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                _buildAppBar(),
+                Text(
+                  "Covid 19",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildAppBar() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        IconButton(
+          icon: Icon(
+            FlutterIcons.menu,
+            color: Colors.white,
+          ),
+          onPressed: null,
+        ),
+        Container(
+          width: 50,
+          height: 50,
+          margin: EdgeInsets.all(16),
+          decoration: BoxDecoration(
+            border: Border.all(color: Colors.white),
+            borderRadius: BorderRadius.all(
+              Radius.circular(100),
+            ),
+            image: DecorationImage(
+              image: AssetImage("assets/images/profile.jpg"),
+            ),
+          ),
+        ),
+      ],
+    );
   }
 }
