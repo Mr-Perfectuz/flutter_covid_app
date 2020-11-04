491e8f46167fc3b12a6effd528167267243e9042
Mr.Perfect <ziyodovanvar1999@gmail.com>
2020-11-04 01:22:56 -0600

    first commit

---
+++ lib/core/consts.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color backgroundColor = Color(0XFFefedf2);
+  static final Color mainColor = Color(0XFF8d12fe);
+}
---
+++ lib/pages/intro_page.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_covid_app/core/consts.dart';
+
+class IntroPage extends StatefulWidget {
+  @override
+  _IntroPageState createState() => _IntroPageState();
+}
+
+class _IntroPageState extends State<IntroPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                AppColors.mainColor,
+                AppColors.mainColor.withOpacity(.1),
+              ],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+            ),
+          ),
+          child: Stack(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.only(top: 50),
+                child: Align(
+                    alignment: Alignment.topCenter,
+                    child: Image.asset("assets/images/logo.png")),
+              ),
+              Positioned(
+                bottom: 50,
+                child: Container(
+                  width: MediaQuery.of(context).size.width
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "Coronavirus disease (Covid-19)",
+                        style: TextStyle(
+                          fontSize: 26,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        "is and infectianus disease caused by a new\nvirus.",
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Colors.white,
+                          height: 1.5,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              )
+            ],
+          )),
+    );
+  }
+}
